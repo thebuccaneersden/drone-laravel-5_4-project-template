931486f2f062135a6190c42361e240abd9c0a3fb
Hans-Christian Andersen <hans.andersen@phulse.com>
2017-04-04 11:52:22 -0700

    Fixing drone build step

---
+++ debug_wait_for_webserver.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+echo "Launching script"
+
+set -e
+
+echo "---"
+
+wait_for_port() {
+  echo "- function start"
+
+  local port=$1
+  count=0
+  while true; do
+    ((count++))
+    echo "- curling"
+    curl localhost:${port} -I -f > /dev/null
+    if [ "$?" == "0" ]
+    then
+      echo "- [✓] Web Server is up!"
+      exit 0
+    fi
+    echo "- [✗] Connection refused on port $port. Waiting 1 second..."
+    echo $count
+    sleep 1
+  done
+}
+
+echo "Waiting for port 8000"
+wait_for_port 8000
+
